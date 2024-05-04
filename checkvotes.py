8a9ff4a933330dfc8e4d554519adb0351b7ef1ed
ViLotusAsc <holmesvilendes@gmail.com>
2024-05-04 15:31:56 -0300

    Release

---
+++ checkvotes.py
@@ -0,0 +1,41 @@
+import json
+
+with open("votos_database.json", "r") as file:
+    data = json.load(file)
+
+data_list = []
+data_list = list(data.values())[0]
+
+
+data_list2 = list()
+for item in data_list:
+    data_list2.append(item)
+
+allvotes = list()
+
+for i in range(len(data_list2)):
+    allvotes.append(data_list[data_list2[i]]["Voto"])
+
+votosindiv = list()
+for i in range(len(data_list2)):
+    votosindiv.append(data_list[data_list2[i]])
+
+print("""\n\n\n\n ██████╗████████╗██████╗ ███╗   ███╗    ██╗   ██╗██████╗ ███╗   ██╗ █████╗     
+██╔════╝╚══██╔══╝██╔══██╗████╗ ████║    ██║   ██║██╔══██╗████╗  ██║██╔══██╗    
+██║        ██║   ██████╔╝██╔████╔██║    ██║   ██║██████╔╝██╔██╗ ██║███████║    
+██║        ██║   ██╔═══╝ ██║╚██╔╝██║    ██║   ██║██╔══██╗██║╚██╗██║██╔══██║    
+╚██████╗   ██║   ██║     ██║ ╚═╝ ██║    ╚██████╔╝██║  ██║██║ ╚████║██║  ██║    
+ ╚═════╝   ╚═╝   ╚═╝     ╚═╝     ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═""")
+
+
+while True:
+    print("\nVer a contagem de Votos [1]\nVer cada voto (individualmente)[2]\n\nResposta: ")
+    ans = int(input(""))
+
+    if ans == 1: 
+        print("\n")
+        print(f"-----------\nChapa 1: {allvotes.count("Chapa 1")}\nChapa 2: {allvotes.count("Chapa 2")}\n-----------")
+    if ans == 2:
+        print(f"\nVotos:")
+        for voto in votosindiv:
+            print(f"{voto}")
--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame 
-
+import json
+from datetime import datetime
+from pygame import mixer
 
 pygame.init()
-screen = pygame.display.set_mode((1355, 700), pygame.RESIZABLE)
+scrn_size = pygame.display.get_desktop_sizes()
+screen = pygame.display.set_mode((1355, 700), pygame.RESIZABLE, pygame.FULLSCREEN)
+
+mixer.init()
+mixer.music.load("votocomputado.mp3")
+mixer.music.set_volume(1)
 
 # Título e Logo 
 pygame.display.set_caption("URNA ELETRÔNICA - CTPM")
 logo = pygame.image.load("ctpmlogo.png")
 pygame.display.set_icon(logo)
 
-layout = pygame.image.load("urnalayoutnv.jpg")
-layout = pygame.transform.scale(layout, (1355, 700))
+layout = pygame.image.load("image.jpg")
+layout = pygame.transform.scale(layout, (scrn_size[0][0]-100, scrn_size[0][1]-100))
 
 running = True
 counter = 1
 
-a1 = (90, 300)
-a2 = (183, 300)
+a1 = (420, 300)
+a2 = (558, 300)
 a3 = (290, 300)
 a4 = (405, 300)
 
@@ -24,17 +31,21 @@ a4 = (405, 300)
 fonte = pygame.font.SysFont('verdana', 90)
 fonte2 = pygame.font.SysFont('verdana', 50)
 
-def texto(text1, text2, text3, text4, a1, a2, a3, a4, nomechapa):
+def updateVotes():
+    with open("votos_database.json", "r") as file:
+        return json.load(file)
+
+
+def texto(text1, text2, text3, text4, a1, a2, nomechapa, voto_cm):
     textoz = fonte.render(text1, True, (0, 0, 0))
     textoy = fonte.render(text2, True, (0, 0, 0))
-    textox = fonte.render(text3, True, (0, 0, 0))
-    textow = fonte.render(text4, True, (0, 0, 0))
     nmchapa = fonte2.render(nomechapa, True, (0, 0, 0))
+    votocomputado = fonte2.render("Voto Computado!", True, (0, 255, 0))
     screen.blit(textoz, a1)
     screen.blit(textoy, a2)
-    screen.blit(textox, a3)
-    screen.blit(textow, a4)
-    screen.blit(nmchapa, (30, 500))
+    screen.blit(nmchapa, (350, 500))
+    if voto_cm:
+        screen.blit(votocomputado, (650, 500))
     pygame.display.update()
 
 
@@ -44,21 +55,17 @@ nomechapa = ''
 
 chapa = {'nome': [], 'num':[]}
 nomes = ['Chapa 1', 'Chapa 2', 'Chapa 3', 'Chapa 4']
-numeros = [1010, 2020, 3030, 4040]
+numeros = [10, 20, 30, 40]
 for nme in nomes:
     chapa['nome'].append(nme)
 for num in numeros:
     chapa['num'].append(num)
 
 
-def fzrvoto(num1, num2, num3, num4): 
+def fzrvoto(num1, num2): 
     num1 = str(num1) 
     num2 = str(num2) 
-    num3 = str(num3)
-    num4 = str(num4)
-    num1 += num2 
-    num1 += num3
-    num1 += num4
+    num1 += num2
     try:
         return int(num1) 
     except Exception:
@@ -66,8 +73,6 @@ def fzrvoto(num1, num2, num3, num4):
 
 num1 = ''
 num2 = ''
-num3 = ''
-num4 = ''
 
 
 voto = 0
@@ -82,6 +87,8 @@ def num(contador):
     if contador == 4:
         return 4
     
+show_voto = False
+scnds = 0
 
 while running: 
     for event in pygame.event.get():
@@ -95,119 +102,99 @@ while running:
                         num1 = '0'
                     if num(counter) == 2:
                         num2 = '0'
-                    if num(counter) == 3:
-                        num3 = '0'
-                    if num(counter) == 4:
-                        num4 = '0'
                     counter += 1
                 if event.key == pygame.K_1:
                     if num(counter) == 1:
                         num1 = '1'
                     if num(counter) == 2:
                         num2 = '1'
-                    if num(counter) == 3:
-                        num3 = '1'
-                    if num(counter) == 4:
-                        num4 = '1'
                     counter += 1
                 if event.key == pygame.K_2:
                     if num(counter) == 1:
                         num1 = '2'
                     if num(counter) == 2:
                         num2 = '2'
-                    if num(counter) == 3:
-                        num3 = '2'
-                    if num(counter) == 4:
-                        num4 = '2'
                     counter += 1
                 if event.key == pygame.K_3:
                     if num(counter) == 1:
                         num1 = '3'
                     if num(counter) == 2:
                         num2 = '3'
-                    if num(counter) == 3:
-                        num3 = '3'
-                    if num(counter) == 4:
-                        num4 = '3'
                     counter += 1
                 if event.key == pygame.K_4:
                     if num(counter) == 1:
                         num1 = '4'
                     if num(counter) == 2:
                         num2 = '4'
-                    if num(counter) == 3:
-                        num3 = '4'
-                    if num(counter) == 4:
-                        num4 = '4'
                     counter += 1
                 if event.key == pygame.K_5:
                     if num(counter) == 1:
                         num1 = '5'
                     if num(counter) == 2:
                         num2 = '5'
-                    if num(counter) == 3:
-                        num3 = '5'
-                    if num(counter) == 4:
-                        num4 = '5'
                     counter += 1
                 if event.key == pygame.K_6:
                     if num(counter) == 1:
                         num1 = '6'
                     if num(counter) == 2:
                         num2 = '6'
-                    if num(counter) == 3:
-                        num3 = '6'
-                    if num(counter) == 4:
-                        num4 = '6'
                     counter += 1
                 if event.key == pygame.K_7:
                     if num(counter) == 1:
                         num1 = '7'
                     if num(counter) == 2:
                         num2 = '7'
-                    if num(counter) == 3:
-                        num3 = '7'
-                    if num(counter) == 4:
-                        num4 = '7'
                     counter += 1
                 if event.key == pygame.K_8:
                     if num(counter) == 1:
                         num1 = '8'
                     if num(counter) == 2:
                         num2 = '8'
-                    if num(counter) == 3:
-                        num3 = '8'
-                    if num(counter) == 4:
-                        num4 = '8'
                     counter += 1
                 if event.key == pygame.K_9:
                     if num(counter) == 1:
                         num1 = '9'
                     if num(counter) == 2:
                         num2 = '9'
-                    if num(counter) == 3:
-                        num3 = '9'
-                    if num(counter) == 4:
-                        num4 = '9'
-                    counter += 1
+                    counter+=1
                 if event.key == pygame.K_BACKSPACE:
                     counter = 0
                     num1 = ''
                     num2 = ''
-                    num3 = ''
-                    num4 = ''
                     nomechapa = ''
                 if event.key == pygame.K_RETURN:
-                    voto = fzrvoto(num1, num2, num3, num4)
+                    time = datetime.now()
+                    voto = fzrvoto(num1, num2)
                     if voto in chapa['num']:
                         indice = chapa['num'].index(voto)
                         nome = chapa['nome'][indice]
                         nomechapa = nome
-                    else:
-                        nomechapa = 'Voto inválido!'
+                        thevote = updateVotes()
+                        thevote["votos"].update({f"Voto{time.microsecond}": {"Voto":nome, "horario":f"{str(time.hour)}:{time.minute}:{time.second}:{time.microsecond}"}})
+                        with open("votos_database.json", "w") as file:
+                            file = json.dump(thevote, file, indent=4)
+                        scnds = time.second
+                        mixer.music.play()
+                        if show_voto == False:
+                            show_voto = True
+                       
                 
-                
+    if show_voto:
+        scnds_N = datetime.now().second
+        if (scnds_N - 1) > scnds:
+            show_voto = False
+
     mousex, mousey = pygame.mouse.get_pos()
+
+    voto = fzrvoto(num1, num2)
+    if voto in chapa['num']:
+        indice = chapa['num'].index(voto)
+        nome = chapa['nome'][indice]
+        nomechapa = nome
+    else:
+        if voto != None:
+            if len(str(voto)) > 1:
+                nomechapa = "Voto Inválido!"
 
     if num2 == '':
         num3 = ''
@@ -223,8 +210,8 @@ while running:
         nomechapa = ''
 
 
-    screen.blit(layout, (5, 5))
-    texto(num1, num2, num3, num4, a1, a2, a3, a4, nomechapa)
+    screen.blit(layout, (55, 5))
+    texto(num1, num2, num3, num4, a1, a2, nomechapa, show_voto)
     pygame.display.update()
     
 
